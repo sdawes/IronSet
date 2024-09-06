5aeac3b05c5cec7ad44f0086949220bcf03c0abe
Stephen Dawes <stephenrdawes@gmail.com>
2024-09-06 19:39:00 +0100

    Initial Commit

---
+++ IronSet/IronSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronSetApp.swift
+//  IronSet
+//
+//  Created by Stephen Dawes on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct IronSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
